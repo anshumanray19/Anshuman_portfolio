bcbfa050158067c4a7b00ebd709eaabeb206a363
anshumanray19 <118247536+anshumanray19@users.noreply.github.com>
2024-07-22 07:56:25 -0500

    Update Script.js
    JS file updated

--- Script.js
+++ Script.js
@@ -1,10 +1,63 @@
-var words = ['United States', 'India', 'France', 'Germany'];
-var index = 0;
-function changeWord(wordElement) {
-  wordElement.textContent = words[index];
-  index = (index + 1) % words.length; 
+AOS.init({
+    duration: 3000,
+});
+
+$(document).ready(function() {
+$('nav a').on('click', function(event) {
+if (this.hash !== '') {
+    event.preventDefault();
+    const hash = this.hash;
+    $('html, body').animate({
+        scrollTop: $(hash).offset().top
+    }, 800, function() {
+        window.location.hash = hash;
+    });
 }
-var wordElement = document.getElementById('word');
-setInterval(function() {
-  changeWord(wordElement);
-}, 1000);
+});
+});
+
+
+$(document).ready(function() {
+// Check the theme preference from localStorage on page load
+const currentTheme = localStorage.getItem('theme');
+if (currentTheme) {
+$('body').addClass(currentTheme);
+if (currentTheme === 'dark-theme') {
+    $('#themeToggle').prop('checked', true);
+}
+}
+
+// Toggle theme when switch is changed
+$('#themeToggle').change(function() {
+if ($(this).prop('checked')) {
+    $('body').removeClass('light-theme').addClass('dark-theme');
+    localStorage.setItem('theme', 'dark-theme');
+} else {
+    $('body').removeClass('dark-theme').addClass('light-theme');
+    localStorage.setItem('theme', 'light-theme');
+}
+});
+});
+
+//scroll button
+function scrollToTop() {
+window.scrollTo({
+top: 0,
+behavior: 'smooth'
+});
+}
+document.addEventListener('DOMContentLoaded', function () {
+var scrollToTopBtn = document.getElementById('scrollToTopBtn');
+
+function checkScroll() {
+if (window.pageYOffset > 0) {
+    scrollToTopBtn.style.display = 'block';
+} else {
+    scrollToTopBtn.style.display = 'none';
+}
+}
+
+window.addEventListener('scroll', checkScroll);
+
+scrollToTopBtn.addEventListener('click', scrollToTop);
+});
